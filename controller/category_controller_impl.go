88e2a96b5ae46634f1e9580af4034def44a7aefe
midoon <harunkoding@gmail.com>
2023-12-15 21:25:35 +0700

    Feat(controller): create  category controller

---
+++ controller/category_controller_impl.go
@@ -0,0 +1,100 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/midoon/simple-crud-go/helper"
+	"github.com/midoon/simple-crud-go/model/web"
+	"github.com/midoon/simple-crud-go/service"
+)
+
+type CategoryControllerImpl struct {
+	CategoryService service.CategoryService
+}
+
+func (controller *CategoryControllerImpl) Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	// decoder := json.NewDecoder(request.Body)
+	// CategoryUpdateRequest := web.CategoryUpdateRequest{}
+	// err := decoder.Decode(&CategoryUpdateRequest)
+	// helper.PanicIfError(err)
+
+	categoryCreateRequest := web.CategoryCreateRequest{}
+	helper.ReadFromRequestBody(request, &categoryCreateRequest)
+
+	categoryResponse := controller.CategoryService.Create(request.Context(), categoryCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+		Data:   categoryResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *CategoryControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	// decoder := json.NewDecoder(request.Body)
+	// CategoryUpdateRequest := web.CategoryUpdateRequest{}
+	// err := decoder.Decode(&CategoryUpdateRequest)
+	// helper.PanicIfError(err)
+
+	CategoryUpdateRequest := web.CategoryUpdateRequest{}
+	helper.ReadFromRequestBody(request, CategoryUpdateRequest)
+
+	categoryId := params.ByName("categoryId")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	CategoryUpdateRequest.Id = id
+
+	categoryResponse := controller.CategoryService.Update(request.Context(), CategoryUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+		Data:   categoryResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *CategoryControllerImpl) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	categoryId := params.ByName("categoryId")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	controller.CategoryService.Delete(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *CategoryControllerImpl) FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	categoryId := params.ByName("categoryId")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	categoryResponse := controller.CategoryService.FindById(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+		Data:   categoryResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *CategoryControllerImpl) FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	categoryResponses := controller.CategoryService.FindAll(request.Context())
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "Ok",
+		Data:   categoryResponses,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+
+}
