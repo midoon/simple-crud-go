3a0f0dcd13ad4506ab668f0f2ef87f21f55eed33
midoon <harunkoding@gmail.com>
2023-12-30 20:40:25 +0700

    Feat(database): create database migration

--- app/database.go
+++ app/database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/simple_crud_go")
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/simple_crud_go_migration")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
@@ -18,3 +18,11 @@ func NewDB() *sql.DB {
 
 	return db
 }
+
+// cara untuk membuat db migration
+//   migrate create -ext sql -dir [nama direktori dari root project] [nama_file_migration]
+// conoth: migrate create -ext sql -dir db/migrations create_table_category
+
+// Menjalankan DB migration
+// migrate -database ["koneksi database"] -path [folder filel migration] up
+// migrate -database "mysql://root@tcp(localhost:3306)/simple_crud_go_migration" -path db/migrations up
